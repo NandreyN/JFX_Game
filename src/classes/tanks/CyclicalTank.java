ecb61f81d6cf538fe6646fd3faf9425a8e68e826
Андрей <andrey.naumovich@mail.ru>
2018-04-02 22:45:38 +0300

    Cyclical/drum tanks

--- src/classes/tanks/CyclicalTank.java
+++ src/classes/tanks/CyclicalTank.java
@@ -1,4 +1,17 @@
 package classes.tanks;
 
+import classes.tanks.parts.*;
+
 public class CyclicalTank extends AbstractTank {
+    private IGun gun;
+
+    public CyclicalTank(ITurret turret, IChassis chassis, int cooldown) {
+        super(turret, chassis);
+        gun = new CyclicalGun(cooldown);
+    }
+
+    @Override
+    public boolean fire() {
+        return gun.fire();
+    }
 }
--- src/classes/tanks/DrumTank.java
+++ src/classes/tanks/DrumTank.java
@@ -1,4 +1,21 @@
 package classes.tanks;
 
+import classes.tanks.parts.DrumGun;
+import classes.tanks.parts.IChassis;
+import classes.tanks.parts.IGun;
+import classes.tanks.parts.ITurret;
+
 public class DrumTank extends AbstractTank {
+    private IGun gun;
+
+    public DrumTank(ITurret turret, IChassis chassis, int internalCoolDown,
+                    int externalCooldown, int capacity) {
+        super(turret, chassis);
+        gun = new DrumGun(internalCoolDown, externalCooldown, capacity);
+    }
+
+    @Override
+    public boolean fire() {
+        return gun.fire();
+    }
 }
