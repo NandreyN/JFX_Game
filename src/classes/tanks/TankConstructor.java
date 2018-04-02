8f00ec47d2e7a7d7048222d83396aab91c9a0042
Андрей <andrey.naumovich@mail.ru>
2018-04-02 22:53:31 +0300

    Tank creator skeleton

---
+++ src/classes/tanks/TankConstructor.java
@@ -0,0 +1,19 @@
+package classes.tanks;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class TankConstructor {
+    public ITank createDrumTank() {
+        throw new NotImplementedException();
+    }
+
+    public ITank createCyclicalTank() {
+        throw new NotImplementedException();
+    }
+
+    public List<ITank> getStockTanksList() {
+        throw new NotImplementedException();
+    }
+}
